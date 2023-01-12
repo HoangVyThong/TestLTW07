f1ea026cb08367b71d5b1c4b149e576a81b45d3b
HoangVyThong <hoangvythong1@gmail.com>
2023-01-12 14:54:51 +0700

    create project

---
+++ DemoMVC/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Abc()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
